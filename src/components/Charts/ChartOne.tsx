9a0f58bda755a0b7fac78de84a45e918d3ff43d3
Selamawit926 <se.selamawit.elias@gmail.com>
2024-05-16 05:03:22 +0300

    Card and graph updated

--- src/components/Charts/ChartOne.tsx
+++ src/components/Charts/ChartOne.tsx
@@ -145,53 +145,39 @@ const ChartOne: React.FC = () => {
   handleReset;
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
-      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
-        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-          <div className="flex min-w-47.5">
-            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
-              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
-            </span>
-            <div className="w-full">
-              <p className="font-semibold text-primary">Total Revenue</p>
-              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
+    <div className="col-span-12 rounded-lg border md:h-80 xl:h-200 sm:h-100 border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
+      <div className='grid grid-cols-3 divide-x divide-slate-400/25'>
+          <div className="col-span-2">
+            <div className='flex flex-col mr-4 gap-y-2'>
+              <div className='font-medium text-black'>COC Graph</div>
+              <div className='border-t border-t-slate-300'>
+                <div id="chartOne" className="-ml-5">
+                  <ReactApexChart
+                    options={options}
+                    series={state.series}
+                    type="area"
+                    height={230}
+                  />
+                </div>
+              </div>
+              
             </div>
+           
           </div>
-          <div className="flex min-w-47.5">
-            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-secondary">
-              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-secondary"></span>
-            </span>
-            <div className="w-full">
-              <p className="font-semibold text-secondary">Total Sales</p>
-              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full max-w-45 justify-end">
-          <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
-            <button className="rounded bg-white py-1 px-3 text-xs font-medium text-black shadow-card hover:bg-white hover:shadow-card dark:bg-boxdark dark:text-white dark:hover:bg-boxdark">
-              Day
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
-              Week
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
-              Month
-            </button>
-          </div>
-        </div>
-      </div>
 
-      <div>
-        <div id="chartOne" className="-ml-5">
-          <ReactApexChart
-            options={options}
-            series={state.series}
-            type="area"
-            height={350}
-          />
+          <div className="flex flex-col justify-between items-center">
+
+                <div className='font-bold text-black'>
+                  <div>Total COC Created <br/> this month</div>
+                </div>
+
+                <div>
+                  <div className='text-7xl font-bold text-black font-sans'>2460</div>
+                  <div>+23% since last month</div>
+                </div>
+          </div>
         </div>
-      </div>
+  
     </div>
   );
 };
