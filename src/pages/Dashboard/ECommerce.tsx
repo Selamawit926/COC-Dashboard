ad5ef03abe8e7a18f3e03a1cdefb5f5e4d3c3c7d
Selamawit926 <se.selamawit.elias@gmail.com>
2024-05-16 05:02:16 +0300

    Dashboard alignment edited

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -10,45 +10,51 @@ const ECommerce: React.FC = () => {
   return (
     <DefaultLayout>
       <div className="grid grid-cols-12 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-12 2xl:gap-7.5">
-        {/* Left Side */}
         <div className="col-span-12 xl:col-span-8">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-12 2xl:gap-7.5">
-            <div className="col-span-12 xl:col-span-6">
-              <CardDataStats
-                title="Total views"
-                total="$3.456K"
-                rate="0.43%"
-                levelUp children={undefined}              >
-                {/* SVG */}
-              </CardDataStats>
+          <div className="grid grid-cols-1 gap-y-4">
+            <div className='grid grid-cols-5 gap-x-2'>
+              <div>
+                <CardDataStats
+                  title="Total views"
+                  total="$3.456K"
+                  rate="0.43%"
+                  levelUp children={undefined}>
+                </CardDataStats>
+              </div>
+              <div>
+                <CardDataStats
+                  title="Total Profit"
+                  total="$45,2K"
+                  rate="4.35%"
+                  levelUp children={undefined}>
+                </CardDataStats>
+              </div>
+              <div>
+                <CardDataStats
+                  title="Total Product"
+                  total="2.450"
+                  rate="2.59%"
+                  levelUp children={undefined}>
+                </CardDataStats>
+              </div>
+              <div>
+                <CardDataStats
+                  title="Total Users"
+                  total="3.456"
+                  rate="0.95%"
+                  levelDown children={undefined}>
+                </CardDataStats>
+              </div>
+              <div>
+                <CardDataStats
+                  title="Total Users"
+                  total="3.456"
+                  rate="0.95%"
+                  levelDown children={undefined}>
+                </CardDataStats>
+              </div>
             </div>
-            <div className="col-span-12 xl:col-span-6">
-              <CardDataStats
-                title="Total Profit"
-                total="$45,2K"
-                rate="4.35%"
-                levelUp children={undefined}              >
-                {/* SVG */}
-              </CardDataStats>
-            </div>
-            <div className="col-span-12 xl:col-span-6">
-              <CardDataStats
-                title="Total Product"
-                total="2.450"
-                rate="2.59%"
-                levelUp children={undefined}              >
-                {/* SVG */}
-              </CardDataStats>
-            </div>
-            <div className="col-span-12 xl:col-span-6">
-              <CardDataStats
-                title="Total Users"
-                total="3.456"
-                rate="0.95%"
-                levelDown children={undefined}              >
-                {/* SVG */}
-              </CardDataStats>
-            </div>
+
             <div className="col-span-12 xl:col-span-12">
               <ChartOne />
             </div>
@@ -57,17 +63,17 @@ const ECommerce: React.FC = () => {
             </div>
           </div>
         </div>
-        {/* Right Side */}
+
         <div className="col-span-12 xl:col-span-4">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-1 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-            <div className="col-span-12">
-              <ChartTwo />
-            </div>
-            <div className="col-span-12">
-              <ChartThree />
-            </div>
-          </div>
-        </div>
+          <div className="grid grid-cols-1 gap-y-6">
+              <div className="col-span-12">
+                <ChartTwo />
+              </div>
+              <div className="col-span-12">
+                <ChartThree />
+              </div>
+           </div>
+         </div>
       </div>
     </DefaultLayout>
   );
